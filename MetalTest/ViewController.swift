80361387d1e9554564b17df3b5467332329716dd
Volodymyr Shlikhta <volodymyr.shlikhta@eleks.com>
2018-04-24 16:27:52 +0300

    Added cube and presentation of it to screen

--- MetalTest/ViewController.swift
+++ MetalTest/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     var vertexBuffer: MTLBuffer! = nil
     var pipelineState: MTLRenderPipelineState!
     var commandQueue: MTLCommandQueue!
-    var objectToDraw: Triangle!
+    var objectToDraw: Cube!
     // display stuff
     var timer: CADisplayLink!
     
@@ -54,7 +54,7 @@ class ViewController: UIViewController {
     }
     
     private func configureVertexBuffer() {
-        objectToDraw = Triangle(device: device)
+        objectToDraw = Cube(device: device)
     }
     
     private func configureRenderPipeline() {
